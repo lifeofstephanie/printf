971cc13417d1927483843dc381ead92b81741dd6
Martins Nwanakwere <nwa23012@byui.edu>
2023-10-17 08:52:28 -0700

    printf

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to print
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+#endif
