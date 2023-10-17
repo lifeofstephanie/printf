ceb45ce1e68c561ad98707735c9bb8c3f4e9ebf2
lifeofstephanie <stephanieanyanwu605@gmail.com>
2023-10-17 10:33:33 -0700

    task 2

--- main.h
+++ main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stddef.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
 
 /**
  * _putchar - Writes a character to the standard output.
